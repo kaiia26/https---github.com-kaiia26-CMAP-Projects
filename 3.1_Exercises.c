4df99e4c310ab355cafa5df9d00a34f8dc4b7889
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-11-07 21:21:18 -0500

    Starting exercises, finished question 1

---
+++ 3.1_Exercises.c
@@ -0,0 +1,18 @@
+/* 
+
+Key: _ indicates spaces!
+
+ What output do the following calls of printf produce?
+
+1. printf("%6d, %4d", 86, 1040); 
+
+Answer: ____86      # 4 spaces and 2 characters = 6
+Answer: 1040        # 4 characters, no spaces
+
+3. 
+
+
+*/
+
+/* Write calls of printf that display a float variable x in the following formats
+a). Exponential notation; left-justified in a field of size 8; one digit after the decimal point */
