afa0bd458ac7c6940720a115fa2916416f588620
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-11-11 22:27:00 -0500

    Left-justified and right-justified solutions

--- 3.1_Exercises.c
+++ 3.1_Exercises.c
@@ -6,8 +6,8 @@ Key: _ indicates spaces!
 
 1. printf("%6d, %4d", 86, 1040); 
 
-Answer: ____86      # 4 spaces and 2 characters = 6
-Answer: 1040        # 4 characters, no spaces
+- Answer: ____86      # 4 spaces and 2 characters = 6
+- Answer: 1040        # 4 characters, no spaces
 
 3. 
 
@@ -15,4 +15,19 @@ Answer: 1040        # 4 characters, no spaces
 */
 
 /* Write calls of printf that display a float variable x in the following formats
-a). Exponential notation; left-justified in a field of size 8; one digit after the decimal point */
+a). Exponential notation; left-justified in a field of size 8; one digit after the decimal point.
+
+ - Answer: printf("%-8.1e", e);
+
+b) Exponential notation; right-justified in a field of size 10; six digits after the decimal point.
+
+ - Answer: printf("%10.6e", e);
+
+c) Fixed decimal notation; left-justified in a field of size 8; three digits after the decimal point/
+ - Answer: printf("%-8.3f", f)
+
+d) Fixed decimal notation: right-justified in a field of size 6; no digits after the decimal point
+- Answer: printf("%6.0f", f)
+
+*/
+
